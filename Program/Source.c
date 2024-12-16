74d774f69919375fe5833c381e41e4ef8ce21ef6
jiseok08 <heoji0912@gmail.com>
2024-12-16 19:47:29 +0900

    (2024.12.16) 더블 버퍼링

--- Program/Source.c
+++ Program/Source.c
@@ -7,6 +7,49 @@
 #define RIGHT 77
 #define DOWN 80
 
+int screenIndex;
+HANDLE screen[2];
+
+void Initialize()
+{
+	CONSOLE_CURSOR_INFO cursor;
+
+	// 화면 버터를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer;
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		CONSOLE_TEXTMODE_BUFFER,
+		NULL
+		);
+
+	cursor.dwSize = 1;
+	cursor.bVisible = FALSE;
+
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen [screenIndex]);
+
+	screenIndex = !screenIndex;
+}
+
+void Clear()
+{
+	COORD position = { 0,0 };
+	DWORD dword;
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+
+	int width = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+
+
+	FillConsoleOutputCharacter(screen[screenIndex],119, 29);
+}
+
 void Position(int x, int y)
 {
 	// x축과 y축을 설정
@@ -22,6 +65,16 @@ int main()
 	char key = 0;
 	int x = 0;
 	int y = 0;
+
+	CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(handle, &consoleInfo);
+	printf("%d\n", consoleInfo.srWindow.Top);
+	printf("%d\n", consoleInfo.srWindow.Left);
+	printf("%d\n", consoleInfo.srWindow.Right);
+	printf("%d\n", consoleInfo.srWindow.Bottom);
+
 	while (1)
 	{
 		Position(x, y);
@@ -36,34 +89,18 @@ int main()
 
 		switch (key)
 		{
-		case UP: y--;
+		case UP: if (y > 0) y--;
 			break ;
-		case LEFT : x -= 2;
+		case LEFT: if (x > 0)x -= 2;
 			break ;
-		case RIGHT : x += 2;
+		case RIGHT:	if (x < consoleInfo.srWindow.Right - 1)x += 2;
 			break ;
-		case DOWN : y++;
+		case DOWN : if (y < consoleInfo.srWindow.Bottom)y++;
 			break;
 		default: printf("Exception\n");
 			break;
 		}
 
-		if (x < 0)
-		{
-			x += 2;
-		}
-		else if (x > 131)
-		{
-			x -= 2;
-		}
-		if (y < 0)
-		{
-			y++;
-		}
-		else if (y > 41)
-		{
-			y--;
-		}
 		system("cls");
 	}
 
