80be9233fa481630c8cb6a8b20bfcecbdef06a0d
jiseok08 <heoji0912@gmail.com>
2024-12-24 19:46:01 +0900

    포커 3차

--- Poker/Source.c
+++ Poker/Source.c
@@ -9,54 +9,50 @@
 struct Card
 {
 	int num;
-	int fnum;
-	char over;
+	int fnum[SIZE];
+	char over[SIZE];
 	char shape[10];
 };
 
 int main()
 {
-	int mamory[SIZE];
-	char cmamory[SIZE];
-	char smamory[10];
 	srand(time(NULL));
 	int fshape = 0;
 	struct Card Card;
 	{
 		for (int i = 0; i < SIZE; i++)
 		{
+			Card.fnum[i] = 0;
 			Card.num = 0;
-			Card.fnum = 0;
 			Card.num = rand() % 13 + 1;
 			switch (Card.num)
 			{
-				Card.fnum = 0;
-			case 1:
-				Card.over = 'A';
+			case 1:		 
+				Card.over[i] = 'A';
 			break;
-			case 2: Card.fnum = 2;
+			case 2: Card.fnum[i] = 2;
+				break;		 
+			case 3: Card.fnum[i] = 3;
+				break;		 
+			case 4: Card.fnum[i] = 4;
+				break;		 
+			case 5: Card.fnum[i] = 5;
+				break;		 
+			case 6: Card.fnum[i] = 6;
+				break;		 
+			case 7: Card.fnum[i] = 7;
+				break;		 
+			case 8: Card.fnum[i] = 8;
+				break;	 	 
+			case 9: Card.fnum[i] = 9;
 				break;
-			case 3: Card.fnum = 3;
+			case 10: Card.fnum[i] = 10;
 				break;
-			case 4: Card.fnum = 4;
-				break;
-			case 5: Card.fnum = 5;
-				break;
-			case 6: Card.fnum = 6;
-				break;
-			case 7: Card.fnum = 7;
-				break;
-			case 8: Card.fnum = 8;
-				break;	 
-			case 9: Card.fnum = 9;
-				break;
-			case 10: Card.fnum = 10;
-				break;
-			case 11: Card.over = 'J';
-				break;
-			case 12: Card.over = 'Q';
-				break;
-			case 13: Card.over = 'K';
+			case 11: Card.over[i] = 'J';
+				break;		  
+			case 12: Card.over[i] = 'Q';
+				break;		  
+			case 13: Card.over[i] = 'K';
 				break;
 			}
 			fshape = rand() % 4 + 1;
@@ -91,46 +87,38 @@ int main()
 				Card.shape[3] = 'b';
 				Card.shape[4] = '\0';
 			}
-			mamory[i] = Card.fnum;
-			cmamory[i] = Card.over;
-			smamory[i] = Card.shape[1];
-			for (int j = 0; j < SIZE; j++)
-			{
-				for (int k = 0; k < SIZE; k++)
-				{
-					if (smamory[SIZE - j] == smamory[SIZE - k])
-					{
-						if (mamory[SIZE - j] == mamory[SIZE - k])
-						{
-							i--;
-							j--;
-							k--;
-						}
-						else if (cmamory[SIZE - j] == cmamory[SIZE - k])
-						{
-							i--;
-							j--;
-							k--;
-						}
-						else
-						{
 
-						}
-					}
-					else
-					{
+			switch (Card.shape[i])
+			{
+			case 'p': if (Card.fnum[i] || Card.fnum[i - 1] || Card.fnum[i - 2] || Card.fnum[i - 3]|| Card.fnum[i - 4] || Card.fnum[i - 5] || Card.fnum[i - 6])			
+				i--;		
+					break;
+			case 'i': if (Card.fnum[i] || Card.fnum[i - 1] || Card.fnum[i - 2] || Card.fnum[i - 3] || Card.fnum[i - 4] || Card.fnum[i - 5] || Card.fnum[i - 6])			
+				i--;	
+				break;
+			case 'e': if (Card.fnum[i] || Card.fnum[i - 1] || Card.fnum[i - 2] || Card.fnum[i - 3] || Card.fnum[i - 4] || Card.fnum[i - 5] || Card.fnum[i - 6])			
+					break;
+				i--;
+			case 'l': if (Card.fnum[i] || Card.fnum[i - 1] || Card.fnum[i - 2] || Card.fnum[i - 3] || Card.fnum[i - 4] || Card.fnum[i - 5] || Card.fnum[i - 6])
+				i--;
+					break;
+			default: 	
+				if (Card.fnum[i] != 0)
+			{
+				printf("%d번 카드 : %s %d\n", i, Card.shape, Card.fnum[i]);
+			}
+				   else if (Card.fnum[i] == 0)
+			{
+				printf("%d번 카드 : %s %c\n", i, Card.shape, Card.over[i]);
+			}
+				break;
+			}
 
-					}
-				}
-			}
-			if (Card.fnum != 0)
-			{
-				printf("%d번 카드 : %s %d\n", i, Card.shape, Card.fnum);
-			}
-			else if (Card.fnum == 0)
-			{
-				printf("%d번 카드 : %s %c\n", i, Card.shape, Card.over);
-			}
+			
+				
+			
+			
+
 		}
 
 		return 0;
